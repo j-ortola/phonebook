6b04b29f3f635c8015f5295f4a579bce502b66f9
Jordi Ortola <jortola86@gmail.com>
2023-03-09 18:21:23 +0100

    3.22 Lint

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const Person = require('./models/person.js')
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
- 
-  if (error.name === 'CastError')
+
+  if (error.name === 'CastError') {
     return response.status(400).json({ error: 'malformatted id' })
-  else if (error.name === 'ValidationError')
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-  
+  }
+
   next(error)
 }
 
@@ -23,32 +24,33 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors())
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then(result => response.json(result))
-    .catch(error => response.status(500).end())
+    .catch(error => response.status(500).json(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   Person.find({})
     .then(result => response.send(`<p>Phonebook has info for ${result.length} people</p><p>${new Date()}</p>`))
-    .catch(error => response.status(500).end())
+    .catch(error => response.status(500).json(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(result => {
-      if (result)
+      if (result) {
         response.json(result)
-      else
+      } else {
         response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body
-  Person.findByIdAndUpdate(request.params.id, {name, number}, {new : true, runValidators: true, context: 'query'})
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(result => response.json(result))
     .catch(error => next(error))
 })
