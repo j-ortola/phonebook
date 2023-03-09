8f6214b5b8e17b1d37eedddb9c11a2da4f4b0baf
Jordi Ortola <jortola86@gmail.com>
2023-03-09 19:24:55 +0100

    3.22 Lint

--- models/person.js
+++ models/person.js
@@ -7,7 +7,7 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
   .then(result => console.log('connected to MongoDB'))
   .catch(error => console.log('error connecting to MongoDB: ', error.message))
-  
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
