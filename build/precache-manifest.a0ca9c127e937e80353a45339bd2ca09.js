8cb761ec55e43c82dbc34a349756ef97309b0f0a
Jordi Ortola <jortola86@gmail.com>
2023-03-09 11:17:15 +0100

    Frontend & env.PORT

---
+++ build/precache-manifest.a0ca9c127e937e80353a45339bd2ca09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a372faa5658717cf9b9d",
+    "url": "/static/css/main.84f92165.chunk.css"
+  },
+  {
+    "revision": "a372faa5658717cf9b9d",
+    "url": "/static/js/main.a372faa5.chunk.js"
+  },
+  {
+    "revision": "3ef085e6a66c0ddb8d59",
+    "url": "/static/js/1.3ef085e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "28d2f953994602fe4046783a1eef5550",
+    "url": "/index.html"
+  }
+];
