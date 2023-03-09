11f3d387d2f8259a7b12a5ee88cd072923407f98
Jordi Ortola <jortola86@gmail.com>
2023-03-09 18:06:14 +0100

    3.21 Full MongoDB support

---
+++ build/precache-manifest.964b444b15334eed063fe77b2a7421be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc6a04deba7c5f03d8a",
+    "url": "/static/css/main.84f92165.chunk.css"
+  },
+  {
+    "revision": "5cc6a04deba7c5f03d8a",
+    "url": "/static/js/main.5cc6a04d.chunk.js"
+  },
+  {
+    "revision": "3ef085e6a66c0ddb8d59",
+    "url": "/static/js/1.3ef085e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dac05f29d0bba989f7ea1152c91ae64c",
+    "url": "/index.html"
+  }
+];
